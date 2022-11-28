311ebf0630af8f04cb7b1d23c37ba88ccfa8bfcc
Franklyn Dsouza <franklyn.dsouza@shopify.com>
2022-11-28 18:12:13 -0500

    always return false

--- src/main/java/io/trino/tpcds/row/TableRowWithNulls.java
+++ src/main/java/io/trino/tpcds/row/TableRowWithNulls.java
@@ -21,23 +21,7 @@ import static io.trino.tpcds.type.Date.fromJulianDays;
 public abstract class TableRowWithNulls
         implements TableRow
 {
-    public volatile long t1 = 1L;
-    public volatile long t2 = 1L;
-    public volatile long t3 = 1L;
-    public volatile long t4 = 1L;
-    public volatile long t5 = 1L;
-    public volatile long t6 = 1L;
-    public volatile long t7 = 1L;
-
-    public volatile long t15 = 1L;
     private final long nullBitMap;
-    public volatile long t8 = 1L;
-    public volatile long t9 = 1L;
-    public volatile long t10 = 1L;
-    public volatile long t11 = 1L;
-    public volatile long t12 = 1L;
-    public volatile long t13 = 1L;
-    public volatile long t14 = 1L;
     private final GeneratorColumn firstColumn;
 
     protected TableRowWithNulls(long nullBitMap, GeneratorColumn firstColumn)
@@ -46,15 +30,9 @@ public abstract class TableRowWithNulls
         this.firstColumn = firstColumn;
     }
 
-    public long getPadding()
-    {
-        return t1 + t2 + t3 + t4 + t5 + t6 + t7 + t8 + t9 + t10 + t11 + t12 + t13 + t14 + t15;
-    }
-
     private boolean isNull(GeneratorColumn column)
     {
-        long kBitMask = 1L << (column.getGlobalColumnNumber() - firstColumn.getGlobalColumnNumber());
-        return (nullBitMap & kBitMask) != 0;
+        return false;
     }
 
     protected <T> String getStringOrNull(T value, GeneratorColumn column)
